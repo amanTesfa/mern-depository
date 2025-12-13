1637f8d5c13b26ba4ce7fd99b0605009476e60cd
amantesfa1010 <amantesfa1010@gmail.com>
2025-12-13 16:01:19 +0300

    mongodb added

---
+++ backEnd/src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose  from "mongoose";
+export const connectDb=()=>{
+try {
+     mongoose.connect(process.env.Mongo_URL)
+
+     console.log("Connected connecting to db")
+} catch (error) {
+    console.log("Error connecting to db",error)
+    process.exit(1)
+}
+}
+
--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -1,7 +1,12 @@
 import express from "express"
 import notes from "./routes/noteRoutes.js"
+import { connectDb } from "./config/db.js"
+import dotenv from "dotenv"
+dotenv.config()
 const app=express()
 app.use("/api/notes",notes)
-app.listen(5001,()=>{
-    console.log("the app started at port 5001")
+const Port=process.env.Port_Num||5001
+connectDb();
+app.listen(Port,()=>{
+    console.log("the app started at port",Port)
 })
